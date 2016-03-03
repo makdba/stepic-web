c624f18a5ec45d6b922fcc4819a746a325b6b8ac
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-03 15:25:21 +0300

    Init.sh changed

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enables/default
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/ginicorn.d/hello.py
-sudo /etc/init.d/gunicron restart
+sudo /etc/init.d/gunicorn restart

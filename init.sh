19f05b5940863f04f891e580b505d8c4a0195eef
makdba <mikhkozlov@yandex.ru>
2016-03-04 16:26:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enables/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/ginicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart

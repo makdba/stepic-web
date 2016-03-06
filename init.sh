eff6e5278d0ce8ab3ef9dc5b3b92f0fe769ff7b9
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-06 22:03:10 +0300

    init.sh modified (gunicorn restart added)

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/guni-dj.py /etc/gunicorn.d/guni-dj.py
+sudo etc/init.d/gunicorn restart
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

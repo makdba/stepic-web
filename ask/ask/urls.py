94560ff0bf9af445a314f10396a8d1559b2aa0c7
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-06 21:37:09 +0300

    Routes added to urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,5 +8,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+      url(r'^$', 'qa.views.test', name='test'),
+      url(r'^login/', 'qa.views.test', name='test'),
+      url(r'^signup/', 'qa.views.test', name='test'),
+      url(r'^question/\d+/', 'qa.views.test', name='test'),
+      url(r'^ask/', 'qa.views.test', name='test'),
+      url(r'^popular/', 'qa.views.test', name='test'),
+      url(r'^new/', 'qa.views.test', name='test'),
+    # url(r'^admin/', include(admin.site.urls)),
 )

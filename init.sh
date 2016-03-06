ad12c984370d6abc12c5ed6fd0a268b46dfde08b
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-06 21:56:52 +0300

    init.sh modified

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/guni-dj.py /etc/gunicorn.d/guni-dj.py
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

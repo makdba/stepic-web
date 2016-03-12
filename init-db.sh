f182e5124a0778d4b104a917eb4744ed68b31ad0
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-12 20:40:59 +0300

    init-db added

---
+++ init-db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#MySQL initial actions
+mysql -uroot -e"CREATE DATABASE webdb"
+mysql -uroot -e"CREATE USER 'webdbuser'@'localhost' IDENTIFIED BY 'webdb'"
+mysql -uroot -e"GRANT ALL ON webdb.* TO 'webdbuser'@'localhost'"
+ 

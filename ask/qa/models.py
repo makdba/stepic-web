958d6c0977598234eb6920105351f6d66c991356
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-12 21:01:10 +0300

    models added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField(blank=True)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name = 'likes')
+
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeFiels()
+	question = models.ForeignKey(Question)
+	author = models.GoreignKey(User)
+

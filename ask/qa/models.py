4d867bc2dc6af21f102d014bedf6ec298228846b
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-15 21:25:50 +0300

    VIEWS changed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,8 +7,14 @@ class Question(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
 	rating = models.IntegerField(blank=True)
-	author = models.ForeignKey(User)
-	likes = models.ManyToManyField(User, related_name = 'likes')
+	author = models.ForeignKey(User, related_name='question_author_set', null=True, blank=True)
+	likes = models.ManyToManyField(User, related_name = 'question_likes_set')
+
+def get_url(self):
+	return reverse('question_info', kwargs= {'id':self.id})
+
+def __unicode__(self):
+	return self.title
 
 
 class Answer(models.Model):
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator, EmptyPage
+from qa.models import *
 
 def test(request, *args, **kwargs):
 	return HttpResponse('OK')
@@ -15,3 +17,45 @@ def question_info(request,q_id):
 	'question': question,
 	})
 
+def paginate (request, qs):
+	try:
+		limit = int(request.GET.get('limit', 10))
+	except ValueError:
+		limit = 10
+	if limit > 100:
+		limit = 10
+
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueErrir:
+		page = 1
+
+
+	paginator = Paginator(qs, limit)
+	try:
+		page= paginator.page(page)
+	except EmptyPage:
+		page= paginator.page(paginator.num_pages)
+
+	return page
+
+
+
+@require_GET
+def questions(request):
+	page = paginate(request, Question.odjects.order_by('-id')) 
+	return render(request, 'questions.html', {
+		'page':page,
+		'baseurl': '/?page=',
+		'webpage_title': 'New questions',
+	})
+
+
+def popular_questions(request):
+	page = paginate(request, Question.odjects.order_by('-rating')) 
+	return render(request, 'questions.html', {
+		'page':page,
+		'baseurl':'/popular/?page=',
+		'webpage_title':'Popular questions',
+	})
+	

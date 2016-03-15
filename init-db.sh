aa56df8899279deb7192b0e37d87eab83699400b
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-15 14:57:43 +0300

    Init-db

--- init-db.sh
+++ init-db.sh
@@ -4,4 +4,5 @@
 mysql -uroot -e"CREATE DATABASE webdb"
 mysql -uroot -e"CREATE USER 'webdbuser'@'localhost' IDENTIFIED BY 'webdb'"
 mysql -uroot -e"GRANT ALL ON webdb.* TO 'webdbuser'@'localhost'"
+mysql -uroot -e"FLUSH PRIVELEGES;"
  
